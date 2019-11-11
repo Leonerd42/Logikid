3a0e4dda0be4b2524b74c310d4902e67c1188105
Pedro Barros <pedrohnbarros@gmail.com>
2019-11-11 10:26:16 -0300

    adicionando nova pagina de program

--- lib/pages/Program.dart
+++ lib/pages/Program.dart
@@ -10,69 +10,40 @@ class Program extends StatelessWidget {
     return DefaultPageTest(
       childBody:  Column(
           children: <Widget>[
-            Row(crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-              Transform.rotate(angle: 0.2, child:
-                  Container(child: Image.asset('assets/imgs/robo.gif',scale: 2.7,), transform: Matrix4.translationValues(90, 30, 0),),
-              ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 60, right: 20,left: 20,bottom: 20),
+            Container(child: Image.asset('assets/imgs/robo.gif',height: 300,), transform: Matrix4.translationValues(0, 0, 0),),
+            Container(
+               transform: Matrix4.translationValues(15,130, 0),
               child: Flex(
                         direction: Axis.horizontal,
                         children: <Widget>[
                         Row(
                           children: <Widget>[
-                            Container(
-                            child: Padding(
-                                padding: EdgeInsets.only(
-                                    left: 10.0, right: 20, top: 0.0, bottom: 10.0),
-                                child: Button('assets/imgs/add.png', 'Adicionar', 68.0, 68.0,
-                                    'adicionar', changePage)),
-                        ),Container(
-                            child: Padding(
-                                padding: EdgeInsets.only(
-                                    left:65, right: 00, top: 0.0, bottom: 10.0),
-                                child: Button('assets/imgs/program.png', 'Programa', 68.0, 68.0,
-                                    'programa', changePage)),
-                        )],
-                        )
-                        
-                      ],)    ,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 30, right: 20,left: 20,bottom: 20),
-              child: Flex(
-                        direction: Axis.horizontal,
-                        children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Container(
+                          Container(
                             child: Padding(
                                 padding: EdgeInsets.only(
                                     left: 25.0, right: 0, top: 0.0, bottom: 10.0),
-                                child: Button('assets/imgs/sair2.png', 'Sair', 68.0, 68.0,
-                                    'sair', changePage)),
+                                child: Button('assets/imgs/sair2.png', 'Voltar', 68.0, 68.0,
+                                    'voltar', changePage)),
                         ),Container(
                             child: Padding(
                                 padding: EdgeInsets.only(
                                     left:105, right: 10, top: 0.0, bottom: 10.0),
                                 child: Button('assets/imgs/play.png', 'Executar', 68.0, 68.0,
                                     'executar', changePage)),
-                        )],
-                        )
-                        
-                      ],)    ,
-            )              
-            ],),
+                                 )],
+                        )  
+                        ],
+                      ),
+                    ),          
+            ],
+            ),
           );
   }
 
   void changePage(String page, BuildContext ctx) {
     switch (page) {
-      case 'sair':
-        Navigator.push(ctx, RotationRoute(page: ChooseAction()));
+      case 'voltar':
+        Navigator.pop(ctx);
         //Navigator.of(ctx).pushNamed('/login');
         break;
       case 'adicionar':
