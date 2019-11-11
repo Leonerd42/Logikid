2710b81d4463bbf498f2f07a66910996c73a3112
Pedro Barros <pedrohnbarros@gmail.com>
2019-11-11 10:42:11 -0300

    adicionando novas paginas e funcionalidade do program

--- lib/pages/Program.dart
+++ lib/pages/Program.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:logikid/pages/AddInstruction.dart';
 import 'package:logikid/pages/ChooseAction.dart';
+import 'package:logikid/pages/Execute.dart';
 import './../components/DefaultPageTest.dart';
 import './../components/CustomizedButton.dart';
 import './../components/SlideTransition.dart';
@@ -10,45 +11,76 @@ class Program extends StatelessWidget {
     return DefaultPageTest(
       childBody:  Column(
           children: <Widget>[
-            Container(child: Image.asset('assets/imgs/robo.gif',height: 300,), transform: Matrix4.translationValues(0, 0, 0),),
-            Container(
-               transform: Matrix4.translationValues(15,130, 0),
+            Row(crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+              Transform.rotate(angle: 0.2, child:
+                  Container(child: Image.asset('assets/imgs/robo.gif',scale: 2.7,), transform: Matrix4.translationValues(90, 30, 0),),
+              ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 60, right: 20,left: 20,bottom: 20),
               child: Flex(
                         direction: Axis.horizontal,
                         children: <Widget>[
                         Row(
                           children: <Widget>[
-                          Container(
+                            Container(
+                            child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: 10.0, right: 20, top: 0.0, bottom: 10.0),
+                                child: Button('assets/imgs/add.png', 'Adicionar', 68.0, 68.0,
+                                    'adicionar', changePage)),
+                        ),Container(
+                            child: Padding(
+                                padding: EdgeInsets.only(
+                                    left:65, right: 00, top: 0.0, bottom: 10.0),
+                                child: Button('assets/imgs/program.png', 'Programa', 68.0, 68.0,
+                                    'programa', changePage)),
+                        )],
+                        )
+                        
+                      ],)    ,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 30, right: 20,left: 20,bottom: 20),
+              child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
                             child: Padding(
                                 padding: EdgeInsets.only(
                                     left: 25.0, right: 0, top: 0.0, bottom: 10.0),
-                                child: Button('assets/imgs/sair2.png', 'Voltar', 68.0, 68.0,
-                                    'voltar', changePage)),
+                                child: Button('assets/imgs/sair2.png', 'Sair', 68.0, 68.0,
+                                    'sair', changePage)),
                         ),Container(
                             child: Padding(
                                 padding: EdgeInsets.only(
                                     left:105, right: 10, top: 0.0, bottom: 10.0),
                                 child: Button('assets/imgs/play.png', 'Executar', 68.0, 68.0,
                                     'executar', changePage)),
-                                 )],
-                        )  
-                        ],
-                      ),
-                    ),          
-            ],
-            ),
+                        )],
+                        )
+                        
+                      ],)    ,
+            )              
+            ],),
           );
   }
 
   void changePage(String page, BuildContext ctx) {
     switch (page) {
-      case 'voltar':
-        Navigator.pop(ctx);
+      case 'sair':
+        Navigator.push(ctx, RotationRoute(page: ChooseAction()));
         //Navigator.of(ctx).pushNamed('/login');
         break;
       case 'adicionar':
         Navigator.push(ctx, RotationRoute(page: AddInstruction()));
         break;
+      case 'executar':
+        Navigator.push(ctx, SlideLeftRoute(page: Execute()));
       /*case 'info':
         Navigator.push(ctx, SlideUpRoute(page: InfoPage()));
         break;*/
